98b75047cd1338e73cdc20cd4e4175bcc8c9ef35
jordan <jortregg@atscom.it>
2023-01-27 15:47:47 +0100

    app a caso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MessageDetailComponent } from './pages/message-detail/message-detail.component';
+import { MessagesComponent } from './pages/messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', 
+  component: NavigationComponent, 
+  children: [ 
+    { path: '', component: MessagesComponent},
+    { path: '', component: MessageDetailComponent}
+  ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/message-detail/message-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-detail',
+  templateUrl: './message-detail.component.html',
+  styleUrls: ['./message-detail.component.scss']
+})
+export class MessageDetailComponent {
+
+}
